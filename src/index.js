42551b1023337ccccd81be8f7ee330ac0cf7f7e5
José Ithalo <jose_ithalo@hotmail.com>
2024-01-03 12:13:52 -0300

    Utilização do arquivo de rotas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import routes from './routes.js';
 
 const server = express();
 
 server.use(express.json());
+server.use(routes);
 
 server.listen(3000, function () {
     console.log('Servidor ok...');
