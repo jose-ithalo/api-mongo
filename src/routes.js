9688d5e1a0e2fde87265a52012e7f50f267e246e
José Ithalo <jose_ithalo@hotmail.com>
2024-01-05 11:35:59 -0300

    Rota para cadastro criada

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { showTeste, register } from './controllers/employees.js';
 
 const routes = Router();
 
-routes.get('/', function (req, res) {
-    return res.status(200).json('Arquivo de rotas');
-});
+routes.get('/', showTeste);
+
+routes.post('/employee', register);
 
 export default routes;
